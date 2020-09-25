c81cfee4f4ecffd91f84cca0ce1042015908e34e
Cem <ugurcem.dev@gmail.com>
2020-09-25 19:58:42 +0300

    WIP: added main script to fetch profile and convert to TOML

---
+++ src/main.py
@@ -0,0 +1,19 @@
+## Get latest .csv in downloads
+import os
+import csv
+import toml
+import file_helper as fh
+
+## OS aware downloads folder
+DOWNLOADS_FOLDER = fh.get_download_path()
+
+## assume latest downloaded file is the aws profile csv
+profile_csv = fh.get_newest_csv(DOWNLOADS_FOLDER)
+
+profile_dict = {}
+reader = csv.DictReader(open(profile_csv))
+
+for v in reader:
+    mydict = dict(v)
+
+print(toml.dumps(mydict))
