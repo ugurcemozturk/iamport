45156d539f810e2ca298f205ef125beb7d0d5140
Cem <ugurcem.dev@gmail.com>
2020-09-25 19:58:12 +0300

    added file/folder helper

---
+++ src/file_helper.py
@@ -0,0 +1,25 @@
+def get_newest_csv(path):
+    """Get latest CSV from given path"""
+    files = os.listdir(path)
+    paths = [
+        os.path.join(path, basename) for basename in files if basename.endswith(".csv")
+    ]
+    return max(paths, key=os.path.getctime)
+
+
+def get_download_path():
+    """Returns the default downloads path for linux or windows"""
+    if os.name == "nt":
+        import winreg
+
+        sub_key = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+        downloads_guid = "{374DE290-123F-4565-9164-39C4925E467B}"
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser("~"), "downloads")
+
+
+def to_yaml(items):
+    return yaml.safe_dump(items, allow_unicode=True)
